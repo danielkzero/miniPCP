73f9a9a7e247ad296ecbe829df135fa2e89c3bd1
Daniel S. Ramos <danikzero@hotmail.com>
2025-04-10 10:08:45 -0300

    feat: Módulo de gerenciamento de menus

---
+++ src/views/privado/menus/MenusView.vue
@@ -0,0 +1,48 @@
+<template>
+    <DataTable :data="menu" :columns="columns" classTable="text-xs" :pesquisar="true" @deleteItem="handleDeleteItem"
+        @editItem="handleEditItem" @createItem="handleCreateItem" :actions="true" :filhos="true"
+        :subcolumn_name="subcolumn_name" :subcolumns="subcolumns" />
+</template>
+
+<script>
+import { menu } from '@/dados/SidebarData.json'; // Importa os dados diretamente do JSON
+import DataTable from '@/components/DataTable/index.vue'; // Table component
+export default {
+    components: {
+        DataTable
+    },
+    data() {
+        return {
+            menu, // Define os dados diretamente
+            columns: [
+                { key: "id", label: "ID", type: "text" },
+                { key: "nome", label: "Nome", type: "text" },
+                { key: "icon", label: "Icone", type: "text" },
+                { key: "link", label: "Link", type: "text" },
+                { key: "description", label: "Descrição", type: "text" },
+                { key: 'datas.data_cadastro', label: 'Cadastro', type: 'date' }
+            ],
+            subcolumn_name: "submenu",
+            subcolumns: [
+                { key: "id", label: "ID", type: "text" },
+                { key: "nome", label: "Nome", type: "text" },
+                { key: "link", label: "Link", type: "text" },
+                { key: "description", label: "Descrição", type: "text" }
+            ]
+        };
+    },
+    methods: {
+        handleDeleteItem(id) {
+            console.log(`Menu ${id}`);
+        },
+        handleEditItem(id) {
+            console.log(`Menu ${id} editado`);
+        },
+        handleCreateItem(id) {
+            console.log(`Novo Menu ${id} criado`);
+        }
+    }
+}
+</script>
+
+<style></style>
